92ca8b6a6b4a44b301ba271566ff98d76bae2552
Rudisons <brokolis178@gmail.com>
2022-01-20 14:58:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert(1);
+const vardi=['Avis Lielais','Maigonis Pālis','Ralfs Kalniņš','Jānis Baranka','Līga Timofejeva'];
+const balvas=['Šņabis','Pervij golf','Dators','Naglas','Tualetes Papīrs'];
+const naudaKopa=1000000;//kopējā summa
+let uzvaretajuSkaits=5;
+let rand=Math.random()*5;
+rand=Math.floor(rand);//noapaļo uz leju
+console.log(vardi[rand]);//izvada konsolē
