0353d34dd2eebc0aa4fb52165dc4523a07def282
Rudisons <brokolis178@gmail.com>
2022-01-27 15:12:47 +0200

    bomāri2

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ const vardi=['Avis Lielais','Maigonis Pālis','Ralfs Kalniņš','Jānis Baranka'
 const balvas=['Šņabis','Pervij golf','Dators','Naglas','Tualetes Papīrs'];
 const naudaKopa=1000000;//kopējā summa
 let uzvaretajuSkaits=5;
-let rindas= document.querySelector('.rindas')
+let rindas= document.querySelector('.rindas');
+rindas.innerHTML='';
 for(let i=0;i<uzvaretajuSkaits; i++){
 let rand=Math.random()* vardi.length;
 rand=Math.floor(rand);//noapaļo uz leju
-console.log(vardi[rand]);//izvada konsolē
+let uzvaretajs=vardi[rand];
+rindas.innerHTML +=`
+<tr>
+<td>${i+1}</td>
+<td>${uzvaretajs}</td>
+</tr>`
 }
+
