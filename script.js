28f6ebab258e85cae77da33050a8c38f7b1c0fa8
Rudisons <brokolis178@gmail.com>
2022-01-27 14:38:25 +0200

    treshais

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const vardi=['Avis Lielais','Maigonis Pālis','Ralfs Kalniņš','Jānis Baranka'
 const balvas=['Šņabis','Pervij golf','Dators','Naglas','Tualetes Papīrs'];
 const naudaKopa=1000000;//kopējā summa
 let uzvaretajuSkaits=5;
-let rand=Math.random()*5;
+let rindas= document.querySelector('.rindas')
+for(let i=0;i<uzvaretajuSkaits; i++){
+let rand=Math.random()* vardi.length;
 rand=Math.floor(rand);//noapaļo uz leju
 console.log(vardi[rand]);//izvada konsolē
+}
